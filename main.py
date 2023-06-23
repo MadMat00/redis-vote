5c63154973a9e0d14e6bd37aa6da1e42c1ed40b4
Mattia Rossini <m.rossini@itsrizzoli.it>
2023-06-23 11:15:33 +0200

    login and register functions

--- main.py
+++ main.py
@@ -13,4 +13,43 @@ r = redis.Redis(
     password=os.getenv("REDIS_PASSWORD"),
 )
 
+fernet = Fernet(r.get("ENCRIPTION_KEY"))
 
+def register():
+    user = input("Inserisci username: ")
+    email = input("Inserisci email: ")
+    password = getpass("Inserisci password: ")
+
+    if r.hgetall(email) != {}:
+        raise ValueError("Utente già registrato")
+    else:
+        r.hset(email, mapping={
+            "Username": user,
+            "Password": fernet.encrypt(password.encode())
+        })
+        print(f"Benvenuto, {user}!")
+    return user
+
+
+def login():
+    email = input("Inserisci email: ")
+    password = getpass("Inserisci password: ")
+    if r.hgetall(email) == {}:
+        raise ValueError("Utente non registrato")
+    else:
+        if fernet.decrypt(r.hget(email, "Password")).decode() != password:
+            raise ValueError("Password errata")
+        user = r.hget(email, "Username").decode()
+        print(f"Benvenuto, {user}!")
+    return user
+
+def main():
+    print("1. Login\n2. Registrazione")
+    choice = int(input())
+
+    if choice == 1:
+        user = login()
+    elif choice == 2:
+        user = register()
+    else:
+        raise ValueError("Valore non valido")
