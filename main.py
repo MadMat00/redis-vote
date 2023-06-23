4b20e59de1378789b756d2c42ba887be0a644e9f
Mattia Rossini <m.rossini@itsrizzoli.it>
2023-06-23 12:01:52 +0200

    Commenti funzioni

--- main.py
+++ main.py
@@ -43,15 +43,40 @@ def login():
     return user
 
 def nuova_proposta(user):
+    """
+    Permette all'utente di creare una nuova proposta.
+    L'utente inserisce il titolo e la descrizione della proposta.
+    Un utente può creare più proposte.
+    Non ci possono essere proposte con lo stesso titolo.
+    Si deve vedere chi ha creato la proposta.
+    Al momento della creazione viene aggiunto un voto.
+    La lista dei voti deve essere un set.
+    """
     return
 
 def vota_proposta(user):
+    """
+    Permette all'utente di votare una proposta.
+    L'utente inserisce il titolo della proposta.
+    Un utente può votare più proposte.
+    Non ci possono essere più voti per la stessa proposta da parte dello stesso utente(il set da errore).
+    L'utente non può votare la sua proposta.
+    """
     return
 
 def vedi_proposte(user):
+    """
+    Permette all'utente di vedere le proposte.
+    L'utente può vedere le proposte ordinate per numero di voti (lunghezza del set).    
+    """
     return
 
 def controlla_proposte_simili(proposta):
+    """
+    Controlla se ci sono proposte con lo stesso titolo.
+    Controlla se ci sono proposte con la descrizione simile.
+    Se ci sono proposte simili non permette di creare la proposta ma viene aggiunto alla proposta l'utente che ha creato la proposta simile.
+    """
     return
 
 def main():
